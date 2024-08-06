c6763fe4dba50a21ef297d8581caf100f44846db
ledsouza <leandro.souza.159@gmail.com>
2024-08-06 12:16:13 -0300

    refatorando Time e Colaborador e criada interface para Colaborador

---
+++ src/componentes/Colaborador/index.tsx
@@ -0,0 +1,22 @@
+import { IColaborador } from "../../compartilhado/interfaces/IColaborador";
+import "./Colaborador.css";
+
+interface ColaboradorProps extends IColaborador {
+    corDeFundo: string;
+}
+
+const Colaborador = ({ nome, imagem, cargo, corDeFundo }: ColaboradorProps) => {
+    return (
+        <div className="colaborador">
+            <div className="cabecalho" style={{ backgroundColor: corDeFundo }}>
+                <img src={imagem} alt={nome} />
+            </div>
+            <div className="rodape">
+                <h4>{nome}</h4>
+                <h5>{cargo}</h5>
+            </div>
+        </div>
+    );
+};
+
+export default Colaborador;
