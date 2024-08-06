09c151ddd36379aad39c47003c5fa97e6a95b2ee
ledsouza <leandro.souza.159@gmail.com>
2024-08-06 10:08:35 -0300

    refatorando botao e banner

---
+++ src/componentes/Botao/index.tsx
@@ -0,0 +1,12 @@
+import "./Botao.css";
+import React, { ReactElement } from "react";
+
+interface BotaoProps {
+    children: ReactElement;
+}
+
+const Botao = (props: BotaoProps) => {
+    return <button className="botao">{props.children}</button>;
+};
+
+export default Botao;
